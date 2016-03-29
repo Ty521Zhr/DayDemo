4008e7fd0b1b872c7e9aa220bd56c63990d1dfe6
佟野 <1215353560@qq.com>
2016-03-29 08:57:51 +0800

    正在做欢迎页

---
+++ app/src/main/java/com/lanou/daydemo/WelcomeActivity.java
@@ -0,0 +1,9 @@
+package com.lanou.daydemo;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dllo on 16/3/29.
+ */
+public class WelcomeActivity extends AppCompatActivity {
+}
